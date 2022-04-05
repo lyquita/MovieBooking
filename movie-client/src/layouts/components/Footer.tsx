578966f8c433fa9ce199aaedb9b9e01c48e2651f
hireoochen@gmail.com <hireoochen@gmail.com>
2022-04-05 15:27:46 +0800

    footer

--- movie-client/src/layouts/components/Footer.tsx
+++ movie-client/src/layouts/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
     return(
         <Box sx={{background:'rgba(0, 0, 0, 0.87)', color:'white', display:'flex', justifyContent:'center', padding:'2rem 2rem', flexDirection:'column',}}>
-            <Divider textAlign='left' sx={{marginBottom:'2rem'}}/>
+            <Divider textAlign='left' sx={{marginBottom:'2rem', background:'white'}}/>
             <Typography variant='body1' > © Hireoo 2022 </Typography>
             <Typography variant='caption'> Peace and Love </Typography>
         </Box>
