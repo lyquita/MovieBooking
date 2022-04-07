5c9b96382ec076ed4faac2016dc31a8de40381a8
hireoochen@gmail.com <hireoochen@gmail.com>
2022-04-07 11:54:56 +0800

    showtime banner

--- movie-client/src/components/showtime/Banner.tsx
+++ movie-client/src/components/showtime/Banner.tsx
@@ -1,13 +1,26 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
-const Banner =() => {
-
-    return(
-        <Box sx={{height:'500px'}}>
-            <img src="https://image.tmdb.org/t/p/original/skvI4rYFrKXS73BJxWGH54Omlvv.jpg" alt="" style={{width:'100%', marginTop:'104px'}}/>
-        </Box>
-
-    )
-}
+const Banner = () => {
+  return (
+    <Box
+      sx={{
+        height: "500px",
+        width: "100%",
+        overflow: "hidden",
+        opacity: "0.8",
+        backgroundImage:
+          "url('https://image.tmdb.org/t/p/original/skvI4rYFrKXS73BJxWGH54Omlvv.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display:'flex',
+        alignItems:'flex-end',
+        justifyContent:'center'
+      }}
+    >
+      <img src="" alt="" style={{ maxHeight: "100%" }} />
+      <Typography color="white" paddingBottom={4} fontSize='24px'>Spider Man</Typography>
+    </Box>
+  );
+};
 
 export default Banner;
